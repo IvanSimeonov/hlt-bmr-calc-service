98f726169a7251b72ccbd1866fe6a5d475b578e2
Ivan Simeonov <ivan.simeonov@frox.ch>
2022-08-08 14:26:27 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# hlt-bmr-calc-service
+# Healthy LifeStyle Tracker - Basal Metabolic Rate Calculator Service
+## App Links:
+### [Swagger OpenApi Docs](http://localhost:8084/v3/api-docs/)
+
+### [Swagger OpenApi UI](http://localhost:8084/swagger-ui/index.html)
---
+++ src/main/java/com/ivnsim/hltbmrcalcservice/HltBmrCalcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ivnsim.hltbmrcalcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HltBmrCalcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HltBmrCalcServiceApplication.class, args);
+	}
+
+}
